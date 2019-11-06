91d276c685ec2fd891d7f577bbbdb158f94b56a7
asdsca <a5530960@gmail.com>
2019-11-06 14:52:27 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,3 @@
-yum install sudo curl wget epel-release -y
 yum groupinstall "MATE Desktop" -y
 rpm -Uvh --force http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm --quiet
 yum install remmina remmina-plugins-* -y
