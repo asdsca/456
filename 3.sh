138c3dc4e28ffd3c19364e20ae198eff060ce94e
asdsca <a5530960@gmail.com>
2019-11-06 15:01:41 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,1 +1,10 @@
-bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh) | tee v2ray_ins.log
+yum -y install wget unzip curl
+wget https://github.com/helloxz/ccaa/archive/master.zip
+unzip master.zip && cd ccaa-master && sh ccaa.sh
+cd ~
+chmod +x /etc/rc.d/rc.local
+echo "/usr/sbin/ccaa start" >> /etc/rc.d/rc.local
+curl https://rclone.org/install.sh | sudo bash
+wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
+rclone config
+mkdir /data/GoogleDrive
