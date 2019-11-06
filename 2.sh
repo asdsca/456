6d6b84ea8bf0d38e2e1d2c73ed7a4d003245a900
asdsca <a5530960@gmail.com>
2019-11-06 14:48:10 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,11 +1,1 @@
-yum install sudo curl wget epel-release -y
-yum groupinstall "MATE Desktop" -y
-rpm -Uvh --force http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm --quiet
-yum install remmina remmina-plugins-* -y
-yum install tigervnc-server tigervnc -y
-yum install xrdp -y
-touch ~/.Xclients
-echo "mate-session" > ~/.Xclients
-chmod a+x ~/.Xclients
-systemctl enable xrdp
-systemctl start xrdp
+bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh) | tee v2ray_ins.log
