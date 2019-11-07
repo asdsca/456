b9f5cc830830445d8882f88f4bc07fb11f49a240
asdsca <a5530960@gmail.com>
2019-11-07 11:01:41 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,3 @@
- yum install net-tools
- wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash serverspeeder.sh
+yum install net-tools
+wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash serverspeeder.sh
+reboot
