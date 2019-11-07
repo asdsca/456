4c82b9bc5b1b07c967c56c6b5af1a951df5f36c8
asdsca <a5530960@gmail.com>
2019-11-07 11:15:30 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,7 @@
+curl https://rclone.org/install.sh | sudo bash
+wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
+rclone config
+mkdir /data/GoogleDrive
 systemctl start firewalld
 yum -y install wget unzip curl
 wget https://github.com/helloxz/ccaa/archive/master.zip
@@ -5,7 +9,3 @@ unzip master.zip && cd ccaa-master && sh ccaa.sh
 cd ~
 chmod +x /etc/rc.d/rc.local
 echo "/usr/sbin/ccaa start" >> /etc/rc.d/rc.local
-curl https://rclone.org/install.sh | sudo bash
-wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
-rclone config
-mkdir /data/GoogleDrive
