b388c830cc4c554fb1bcc8f81dd7a43686e6afa5
asdsca <a5530960@gmail.com>
2019-12-12 01:14:16 +0800

    Update and rename 55.sh to 56.sh

--- 56.sh
+++ 56.sh
@@ -1,5 +1,5 @@
 ssl_judge_and_install(){
-    if [[ -f "/data/mycrt.key" && -f "/data/mykey.crt" ]];then
+    if [[ -f "/data/v2ray.key" && -f "/data/v2ray.crt" ]];then
         echo "证书文件已存在"
     fi
 }
